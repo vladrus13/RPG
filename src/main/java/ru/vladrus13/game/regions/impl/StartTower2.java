90e92243cc76190d0eb3f9987a8113c176218d3d
vladrus13 <vladrus13rus@yandex.ru>
2021-04-04 00:10:28 +0300

    Get a factory

--- src/main/java/ru/vladrus13/game/regions/impl/StartTower2.java
+++ src/main/java/ru/vladrus13/game/regions/impl/StartTower2.java
@@ -3,6 +3,7 @@ package ru.vladrus13.game.regions.impl;
 import ru.vladrus13.game.actors.ActorFactory;
 import ru.vladrus13.game.items.ItemFactory;
 import ru.vladrus13.game.regions.RegionFactory;
+import ru.vladrus13.jgraphic.basic.components.Background;
 import ru.vladrus13.jgraphic.basic.components.Choose;
 import ru.vladrus13.jgraphic.basic.components.Filler;
 import ru.vladrus13.jgraphic.basic.components.Text;
@@ -12,6 +13,8 @@ import ru.vladrus13.jgraphic.bean.CoordinatesType;
 import ru.vladrus13.jgraphic.bean.Point;
 import ru.vladrus13.jgraphic.bean.Size;
 import ru.vladrus13.jgraphic.exception.GameException;
+import ru.vladrus13.jgraphic.factory.components.ButtonFactory;
+import ru.vladrus13.jgraphic.factory.components.TextFactory;
 import ru.vladrus13.jgraphic.property.MainProperty;
 import ru.vladrus13.rpg.basic.direction.Direction;
 import ru.vladrus13.rpg.basic.event.region.RegionEventFocused;
@@ -36,14 +39,19 @@ public class StartTower2 {
     private static void setActors(Region region) throws GameException {
         Actor pirate = ActorFactory.createActor("pirate", new Point(6L * tileSize, 4L * tileSize), region);
         Choose pirateChoose;
+        Size fullSize = new Size(1000, 1000, CoordinatesType.RATIO);
+        Point fullStart = new Point(0, 0, CoordinatesType.RATIO);
+        ButtonFactory buttonFactory = new ButtonFactory()
+                .setBackground(new Background("back", fullStart.copy(), fullSize.copy(), new Filler(Color.WHITE), null))
+                .setChooseBackground(new Background("chBack", fullStart.copy(), fullSize.copy(), new Filler(Color.LIGHT_GRAY), null));
+        TextFactory textFactory = new TextFactory()
+                .setNameFont("Inventory")
+                .setColor(Color.BLACK)
+                .setFontSize(new Size(300, 0, CoordinatesType.RATIO))
+                .setTextAlign(Text.TextAlign.CENTER);
         pirateChoose = Choose.getInstance("pirateChoose", 3, new Point(100, 100, CoordinatesType.RATIO),
                 new Size(800, 800, CoordinatesType.RATIO), region, new Size(800, 100, CoordinatesType.RATIO),
-                new Filler[]{new Filler(Color.WHITE), new Filler(Color.WHITE), new Filler(Color.WHITE)},
-                new Filler[]{new Filler(Color.LIGHT_GRAY), new Filler(Color.LIGHT_GRAY), new Filler(Color.LIGHT_GRAY)},
-                new String[]{"Hello", "Shop", "Bye"}, "Inventory", new Size(400, 0, CoordinatesType.RATIO), Color.BLACK,
-                Text.TextAlign.CENTER, new Event[]{new IntEvent(IntEvent.NOTHING), new IntEvent(IntEvent.NOTHING), new IntEvent(IntEvent.NOTHING)},
-                new Event[]{new IntEvent(IntEvent.NOTHING), new IntEvent(IntEvent.NOTHING), new IntEvent(IntEvent.NOTHING)}
-        );
+                new String[]{"Hello", "Shop", "Bye"}, buttonFactory, textFactory);
         pirate.setOnTrigger(new RegionEventFocused(pirateChoose, true, false));
         region.setActors(new ArrayList<>(Collections.singletonList(pirate)));
     }
