e76caa2ececee2d5a04e001f9e5ca4fd4cbdb05d
vladrus13 <vladrus13rus@yandex.ru>
2021-05-06 11:11:25 +0300

    Start battle work

--- core/src/java/ru/vladrus13/rpg/battle/BattleStatus.java
+++ core/src/java/ru/vladrus13/rpg/battle/BattleStatus.java
@@ -1,4 +1,18 @@
 package ru.vladrus13.rpg.battle;
 
-public class BattleStatus {
+import ru.vladrus13.graphic.Graphics;
+import ru.vladrus13.jgraphic.basic.Frame;
+import ru.vladrus13.jgraphic.bean.CoordinatesType;
+import ru.vladrus13.jgraphic.bean.Point;
+import ru.vladrus13.jgraphic.bean.Size;
+
+public class BattleStatus extends Frame {
+    public BattleStatus(String name, Frame parent) {
+        super(name, new Point(0, 800, CoordinatesType.RATIO), new Size(1000, 200, CoordinatesType.RATIO), parent);
+    }
+
+    @Override
+    protected void nonCheckingDraw(Graphics graphics) {
+
+    }
 }
