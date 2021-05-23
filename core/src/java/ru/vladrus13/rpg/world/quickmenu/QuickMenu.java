18e9248b7561aa94e79172edbb85b97944a523b1
vladrus13 <vladrus13rus@yandex.ru>
2021-05-23 12:52:32 +0300

    Fix quick menu

--- core/src/java/ru/vladrus13/rpg/world/quickmenu/QuickMenu.java
+++ core/src/java/ru/vladrus13/rpg/world/quickmenu/QuickMenu.java
@@ -30,17 +30,17 @@ public class QuickMenu extends Frame implements Reloaded {
         Size fullSize = new Size(1000, 1000, CoordinatesType.RATIO);
         Point fullStart = new Point(0, 0, CoordinatesType.RATIO);
         ButtonFactory buttonFactory = new ButtonFactory()
-                .setChooseBackground(new Background("background", fullStart.copy(), fullSize.copy(), new Filler(Color.BLUE.darker().darker()), null))
-                .setBackground(new Background("background", fullStart.copy(), fullSize.copy(), new Filler(Color.BLUE.darker()), null));
+                .setChooseBackground(new Background("background", fullStart.copy(), fullSize.copy(), new Filler(Color.BLUE.darker()), null))
+                .setBackground(new Background("background", fullStart.copy(), fullSize.copy(), new Filler(Color.BLUE), null));
         TextFactory textFactory = new TextFactory()
                 .setTextAlign(Text.TextAlign.LEFT)
-                .setFontSize(new Size(300, 0, CoordinatesType.RATIO))
-                .setNameFont("PixelFontGame")
+                .setFontSize(new Size(900, 0, CoordinatesType.RATIO))
+                .setNameFont("MK-90")
                 .setColor(Color.BLACK);
         try {
-            Size buttonSize = new Size(1000, 200, CoordinatesType.RATIO);
-            choose1 = Choose.getInstance("quick", 2, new Point(20, 20, CoordinatesType.RATIO),
-                    new Size(500, 500, CoordinatesType.RATIO), this, buttonSize.copy(),
+            Size buttonSize = new Size(1000, 500, CoordinatesType.RATIO);
+            choose1 = Choose.getInstance("quick", 2, new Point(50, 100, CoordinatesType.RATIO),
+                    new Size(300, 100, CoordinatesType.RATIO), this, buttonSize.copy(),
                     new String[]{"Game", "Exit"}, new Event[]{new WorldEventChange(WorldEventChange.ChangeWorldFrame.REGION),
                             new GameEventChange(GameEventChange.ChangeWorldFrame.MENU)}, new Event[]{null, null}, buttonFactory, textFactory);
         } catch (AppException e) {
