36d7e2267a85f8ab3ddd6ef5a4a1197a02393731
vladrus13 <vladrus13rus@yandex.ru>
2021-05-28 12:30:50 +0300

    Add battle frame

--- core/src/java/ru/vladrus13/rpg/battle/Battle.java
+++ core/src/java/ru/vladrus13/rpg/battle/Battle.java
@@ -1,4 +1,33 @@
 package ru.vladrus13.rpg.battle;
 
-public class Battle {
+import ru.vladrus13.graphic.Graphics;
+import ru.vladrus13.jgraphic.basic.Frame;
+import ru.vladrus13.jgraphic.basic.UpdatedFrame;
+import ru.vladrus13.jgraphic.bean.CoordinatesType;
+import ru.vladrus13.jgraphic.bean.Point;
+import ru.vladrus13.jgraphic.bean.Size;
+import ru.vladrus13.rpg.basic.Reloaded;
+
+public class Battle extends UpdatedFrame implements Reloaded {
+
+    BattleStatus battleStatus;
+
+    public Battle(String name, Frame parent) {
+        super(name, new Point(0, 0, CoordinatesType.RATIO), new Size(1000, 1000, CoordinatesType.RATIO), parent);
+    }
+
+    @Override
+    protected void nonCheckingUpdate(long delay) {
+
+    }
+
+    @Override
+    protected void nonCheckingDraw(Graphics graphics) {
+
+    }
+
+    @Override
+    public void reload() {
+
+    }
 }
